3d87dafc11b3c3458941287be815405a969ad2ea
Lakshay Jain <lakshayjain24@gmail.com>
2024-09-16 20:46:21 +0530

    added files for routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const passportAuthenticate = require("../middlewares/JWT.middleware");
 const { getUser} = require("../controllers/User.Controller");
-router.get("/", getUser);
+
+router.get("/:userId",passportAuthenticate(), getUser);
 
 
 module.exports = router

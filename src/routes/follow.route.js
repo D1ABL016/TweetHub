3d889c783b962881845053efdd852d30294150b6
Lakshay Jain <lakshayjain24@gmail.com>
2024-09-23 09:28:05 +0530

    added routes for followendpoint

--- src/routes/follow.route.js
+++ src/routes/follow.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const follow = require("../controllers/Follow.Controller");
+const follow = require("../controllers/Follow.Controller").follow;
+const Unfollow = require("../controllers/Follow.Controller").Unfollow;
 const passportAuthenticate = require("../middlewares/JWT.middleware");
 
 router.post("/",passportAuthenticate(), follow);
-
+router.delete("/",passportAuthenticate() , Unfollow);
 
 module.exports = router
